945b56c6ef30cd755e339ba3ec2ceda803f8f8bd
JuanValencia <juan.j.valencia@correounivalle.edu.co>
2026-04-14 21:38:22 -0500

    Commit inical

---
+++ src/main/java/com/turny/ApiTurny/ApiTurnyApplication.java
@@ -0,0 +1,13 @@
+package com.turny.ApiTurny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTurnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTurnyApplication.class, args);
+	}
+
+}
